3401778350431955dfef044e09beddfa84fb068d
Bilel Sahraoui <bsahraoui.mail@gmail.com>
2023-01-30 17:33:59 +0100

    middleware

--- src/middlewares/verifyCompanyRegistered.js
+++ src/middlewares/verifyCompanyRegistered.js
@@ -2,8 +2,8 @@ const User = require('../models/user.model');
 
 async function verifyCompanyRegistered(req, res, next){
   const verifyUser = await User.findById(req.userToken.id).populate('company');
-  if(verifyUser.company){
-    return res.status(401).send({ message: "Cannot create a company, company already registered" });
+  if(verifyUser.freelance){
+    return res.status(401).send({ message: "Cannot create a company as freelancer" });
   }else{
     next();
   }
--- src/middlewares/verifyFreelanceRegistered.js
+++ src/middlewares/verifyFreelanceRegistered.js
@@ -2,9 +2,8 @@ const User = require('../models/user.model');
 
 async function verifyFreelanceRegistered(req, res, next) {
   const verifyUser = await User.findById(req.userToken.id).populate('freelance');
-  if(verifyUser.freelance){
-    console.log(verifyUser)
-    return res.status(401).send({ message: "Cannot create a freelance, freelance already registered" });
+  if(verifyUser.company){
+    return res.status(401).send({ message: "Cannot create a freelance as company" });
   }else{
     next();
   }
